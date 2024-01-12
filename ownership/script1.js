f3e03bf5e86222d0a521fbd09942dee6d4942e26
Aaditya785 <aadityasandil5@gmail.com>
2024-01-12 18:25:00 +0530

    added ownership code logic

--- ownership/script1.js
+++ ownership/script1.js
@@ -8,7 +8,30 @@ $(document).ready(function() {
     $('[data-bs-toggl="tooltip"]').on('click', function() {
       $(this).tooltip('hide');
     });
+
+    $(".addEntity").on("click", function() {
+      // Clone the first row
+      var newRow = $("tbody tr:first").clone();
+
+      // Make the new row editable
+      newRow.find("[contenteditable]").attr("contenteditable", "true");
+
+      // Append the new row to the table
+      $("tbody").append(newRow);
+    });
+
+    // Save Entity Button Click Event
+    $(".saveEntity").on("click", function() {
+      // Make all contenteditable elements non-editable
+      $("[contenteditable]").attr("contenteditable", "false");
+    });
   });
+
+   $('input[type="radio"]').on("click", function () {
+    console.log($(this).attr("id"))
+    var hideRow = $(this).attr("id");
+    $("."+hideRow).hide();
+   })
 
 
 function selectCategory(category) {
